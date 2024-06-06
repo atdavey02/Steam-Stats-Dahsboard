3c4ae595d08a49a87286d3babc0087ea871db188
Anthony Davey <anthonydavey0606@gmail.com>
2024-06-06 12:18:32 +1000

    Added Months Page To Interface

--- steamInterface.py
+++ steamInterface.py
@@ -3,13 +3,27 @@ from steamGraphs import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 
+monthsDict = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December"  
+}
 
 class Homepage(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setMinimumSize(QSize(1300, 700))
-        self.SteamID= QLineEdit()
-        self.APIID = QLineEdit()
+        self.SteamID= QLineEdit("")
+        self.APIID = QLineEdit("")
         self.enterButton = QPushButton("Enter")
         self.welcomeLabel = QLabel()
         self.welcomeLabel.setText("Welcome to the Steam Stats Dashboard!")
@@ -67,6 +81,9 @@ class Homepage(QMainWindow):
         self.statsHeader.setFont(loadingFont)
         self.achievementsLabel = QLabel("See Acheivements By Game")
         self.monthsLabel = QLabel("See Achievments by Month")
+        self.monthsButton = QPushButton("Here")
+        self.monthsButton.setStyleSheet("color: white; background-color: #096295")
+        self.monthsButton.clicked.connect(lambda: loadMonths())
         self.playersLabel = QLabel("See How Popular Your Owned Games Are")
         self.playersButton = QPushButton("Here")
         self.playersButton.setStyleSheet("color: white; background-color: #096295")
@@ -75,8 +92,9 @@ class Homepage(QMainWindow):
         statsLayout.addWidget(self.statsHeader,0,0)
         statsLayout.addWidget(self.achievementsLabel,1,0)
         statsLayout.addWidget(self.monthsLabel,2,0)
+        statsLayout.addWidget(self.monthsButton,2,1)
         statsLayout.addWidget(self.playersLabel,3,0)
-        statsLayout.addWidget(self.playersButton,4,1)
+        statsLayout.addWidget(self.playersButton,3,1)
         statsContainer = QWidget()
         statsContainer.setLayout(statsLayout)
         statsContainer.setStyleSheet("color: white; background-color: #161920")
@@ -88,6 +106,14 @@ class Homepage(QMainWindow):
         playersLayout = QVBoxLayout()
         playersLayout.addWidget(self.playersHeader)
         playersLayout.addWidget(self.top5Label)
+
+        self.monthsHeader = QLabel("What Month Did You Get The Most Achievements In?")
+        self.monthsHeader.setStyleSheet("background-color: #096295; min-height: 80px;")
+        self.monthsHeader.setFont(loadingFont)
+        self.top5MonthsLabel = QLabel("Here Are Your 5 Most Active Months on Steam:")
+        monthsLayout = QVBoxLayout()
+        monthsLayout.addWidget(self.monthsHeader)
+        monthsLayout.addWidget(self.top5MonthsLabel)
 
 
         def loadNext():
@@ -115,6 +141,19 @@ class Homepage(QMainWindow):
             playersContainer.setStyleSheet("color: white; background-color: #161920")
             self.setCentralWidget(playersContainer)
             self.setWindowTitle("Games by Popularity")
+
+        def loadMonths ():
+            df = pd.read_csv('achievementsmonth.csv',encoding = "ISO-8859-1")
+            df = df.sort_values(by=["Achievement Amount"],ascending=False)
+            df = df.head(5)
+            for row in df.iterrows():
+                self.newLabel = QLabel(monthsDict[row[1]["Month"][5:]] + " " + row[1]["Month"][:4] + ": " + str(row[1]["Achievement Amount"]))
+                monthsLayout.addWidget(self.newLabel)
+            monthsContainer = QWidget()
+            monthsContainer.setLayout(monthsLayout)
+            monthsContainer.setStyleSheet("color: white; background-color: #161920")
+            self.setCentralWidget(monthsContainer)
+            self.setWindowTitle("Top Months")
             
 
 
@@ -127,7 +166,7 @@ class dataCollector(QThread):
     def run(self):
         generateGraphs(self.user,self.key)
         getPlayerAmounts(self.user,self.key)
-      
+
 
 app = QApplication([])
 
